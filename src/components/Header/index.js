912b9ac6712c87e92c32d20b0512a1d9667ef67f
bhudev <tushareshukla@gmail.com>
2024-05-01 11:53:29 +0530

    header-setup-muiconfigure

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div className='header-left'>
+            <img src='https://www.pngkey.com/png/detail/134-1346237_google-logo-png-transparent-png.png' alt='logo'/>
+            <div className='header-search'>
+                <input type='text' placeholder='Search'/>   
+            </div>
+
+        </div>
+        <div className='header-right'>
+            
+            </div>
+    </div>
+  )
+}
+
+export default Header
